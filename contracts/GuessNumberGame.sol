28eee1773bcf1e648aa84ef2f4ab45c5000408c4
Tommy <tommy50101@gmail.com>
2022-12-14 21:47:25 +0800

    Add airnode for random number

--- contracts/GuessNumberGame.sol
+++ contracts/GuessNumberGame.sol
@@ -1,13 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract GuessNumberGame {
+import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract GuessNumberGame is RrpRequesterV0, Ownable  {
+    // Defining the Parameters for the request
+    bytes public parameters = abi.encode(
+        bytes32("1SSSS"),  // 這行定義有幾個 parameters，例如這邊有4個，就是4個S
+        bytes32("symbol"),
+        "TSLA",
+        bytes32("event"),
+        "Trade",
+        bytes32("_path"),
+        "Trade.TSLA.price",
+        bytes32("_type"),
+        "int256"
+    ); 
+
     // Global Variables
     uint256 public endTime; // 目前輪次結束時間
     uint256 public interval; // 輪次間隔時長
     uint256 public ticketPrice = 0.0001 ether; // 門票
     uint256 public round = 1; // 當前輪次
     uint256 public pot = 0; // 當前輪次彩池獎金
+
+    address public constant airnodeAddress = 0x4f15d2ECFc6b960eB5a4C22075788e7Ac9326437;
+    bytes32 public constant endpointId = 0x070b496e7aa9aff248b803c7c3ac6a55f52893ae14f0d841c9ccf3b205ca5865;
+    address payable public sponsorWallet;
     
     uint256 public constant MAX_NUMBER = 10000; // 最大號碼
 
@@ -17,13 +38,27 @@ contract GuessNumberGame {
     // Mappings
     mapping(uint256 => mapping(uint256 => address[])) public tickets; // 輪次 => 號碼 => [選這號碼的地址們]
     mapping(uint256 => uint256) public winningNumber; // 輪次 => 該輪中獎號碼
+    mapping(bytes32 => bool) public pendingRequestIds; // mapping to store pending request ids
+
+     // Events
+    event RequestedRandomNumber(bytes32 indexed requestId);
+    event ReceivedRandomNumber(bytes32 indexed requestId, uint256 randomNumber);
 
     /// @param _endTime 第一輪結束時間
     /// @param _interval 輪次間隔時長
-    constructor(uint256 _endTime, uint256 _interval) {
-        require(_endTime > block.timestamp, 'End time must be in the future');
-        endTime = _endTime;
+    constructor(uint256 _endTime, uint256 _interval, address _airnodeRrpAddress)
+        RrpRequesterV0(_airnodeRrpAddress)
+    {
+        if (_endTime <= block.timestamp) revert EndTimeReached(_endTime);
+        endTime = _endTime; // store the end time of the lottery
         interval = _interval;
+    }
+
+    function setSponsorWallet(address payable _sponsorWallet)
+        external
+        onlyOwner
+    {
+        sponsorWallet = _sponsorWallet;
     }
 
     /// @notice 處理轉進來的錢
@@ -48,21 +83,48 @@ contract GuessNumberGame {
         pot += ticketPrice;
     }
 
-    /// @notice 超過本輪結束時間後，可由任何人呼叫，並帶入一組數字做為中獎號碼
-    /// @param _randomNumber 中獎號碼
-    function closeRound(uint256 _randomNumber) external {
-        require(block.timestamp > endTime, 'Game has not ended');
+    //
+    function getWinningNumberAndCloseRound() external payable {
+        require(msg.value >= 0.01 ether, "Please top up sponsor wallet"); // user needs to send 0.01 ether with the transaction
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnodeAddress,
+            endpointId,
+            address(this),
+            sponsorWallet,
+            address(this),
+            this.closeRound.selector,
+            ""
+        );
+        pendingRequestIds[requestId] = true;
+        emit RequestedRandomNumber(requestId);
+        sponsorWallet.call{value: msg.value}(""); // Send funds to sponsor wallet
+    }
+
+    // 給 rrp 回調的函式
+    function closeRound(bytes32 requestId, bytes calldata data)
+        external
+        onlyAirnodeRrp
+    {
+        require(pendingRequestIds[requestId], "No such request made");
+        delete pendingRequestIds[requestId];
+
+        uint256 _randomNumber = abi.decode(data, (uint256)) % MAX_NUMBER;
+        emit ReceivedRandomNumber(requestId, _randomNumber);
+
         winningNumber[round] = _randomNumber;
         address[] memory winners = tickets[round][_randomNumber];
-        round++;
-        endTime += interval;
+        unchecked {
+            ++round;
+        }
+        endTime +=  1 days;
         if (winners.length > 0) {
-            // 平分本輪彩金給所有中獎者
             uint256 earnings = pot / winners.length;
-            pot = 0;
-            for (uint256 i = 0; i < winners.length; i++) {
-                (bool success, ) = payable(winners[i]).call{value: earnings}('');
-                require(success, "receiver rejected ETH transfer");
+            pot = 0; // reset pot
+            for (uint256 i = 0; i < winners.length; ) {
+                payable(winners[i]).call{value: earnings}(""); 
+                unchecked {
+                    ++i;
+                }
             }
         }
     }
--- test/GuessNumberGame.js
+++ test/GuessNumberGame.js
@@ -1,19 +1,35 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+const airnodeProtocol = require('@api3/airnode-protocol');
+const airnodeAdmin = require('@api3/airnode-admin');
 
 describe('GuessNumberGame', function () {
+    let guessNumberGameContract, accounts, nextWeek;
     let interval = 86400;
-    let endTime;
-    let accounts;
-    let guessNumberGameContract;
 
     describe('Deployment', function () {
         it('Deploys', async function () {
-            const guessNumberGameFactory = await ethers.getContractFactory('GuessNumberGame');
+            const GuessNumberGame = await ethers.getContractFactory('GuessNumberGame');
             accounts = await ethers.getSigners();
-            endTime = Math.floor(Date.now() / 1000) + interval;
-            guessNumberGameContract = await guessNumberGameFactory.deploy(endTime, interval);
+            nextWeek = Math.floor(Date.now() / 1000) + 86400;
+
+            let { chainId } = await ethers.provider.getNetwork(); // Get the chainId we are using in hardhat
+            const rrpAddress = airnodeProtocol.AirnodeRrpAddresses[chainId]; // Get the AirnodeRrp address for the chainId
+
+            guessNumberGameContract = await GuessNumberGame.deploy(nextWeek, 86400, rrpAddress);
             expect(await guessNumberGameContract.deployed()).to.be.ok;
+        });
+
+        it('Sets sponsor wallet', async function () {
+            const sponsorWalletAddress = await airnodeAdmin.deriveSponsorWalletAddress(
+                'xpub6DF9CEitAcAzWmdCpSJ3nmc18L1NDzCMXtX81eGCiScdkgcV1G1Xo7tYFnVsqNZSurAwPKpKs3W2VXKyBytLg89VSvD1Rh4JxchMBTCLxjC',
+                '0x4f15d2ECFc6b960eB5a4C22075788e7Ac9326437',
+                guessNumberGameContract.address
+            );
+            await expect(guessNumberGameContract.connect(accounts[1]).setSponsorWallet(sponsorWalletAddress)).to.be.reverted; // onlyOwner
+            await guessNumberGameContract.setSponsorWallet(sponsorWalletAddress);
+
+            expect(await guessNumberGameContract.sponsorWallet()).to.equal(sponsorWalletAddress);
         });
 
         it('Has the correct endTime', async function () {
@@ -23,38 +39,31 @@ describe('GuessNumberGame', function () {
     });
 
     describe('GuessNumberGame is open', function () {
-        it('Users should be able to enter any number between 1-10000', async function () {
+        it('Users enter between 1-3', async function () {
             for (let account of accounts) {
-                let randomNumber = Math.floor(Math.random() * 10000);
+                let randomNumber = Math.floor(Math.random() * 3);
                 await guessNumberGameContract.connect(account).enter(randomNumber, { value: ethers.utils.parseEther('0.0001') });
                 const entries = await guessNumberGameContract.getEntriesForNumber(randomNumber, 1);
                 expect(entries).to.include(account.address);
             }
         });
 
-        it('Should fail if entry money not correct', async function () {
-            await expect(guessNumberGameContract.connect(accounts[0]).enter(4, { value: ethers.utils.parseEther('0.02') })).to.be.reverted;
-        });
-
-        it('Should fail if entry number greater than max number', async function () {
-            await expect(guessNumberGameContract.connect(accounts[0]).enter(88888, { value: ethers.utils.parseEther('0.01') })).to.be.reverted;
-        });
-
-        it('Should fail to close guessNumberGameContract if round is still open', async function () {
-            await expect(guessNumberGameContract.connect(accounts[0]).closeRound(55)).to.be.reverted;
+        it('Should fail if entry is invalid', async function () {
+            await expect(guessNumberGameContract.connect(accounts[0]).enter(4, { value: ethers.utils.parseEther('0.02') })).to.be.reverted; // exact ticket price only
+            await expect(guessNumberGameContract.connect(accounts[0]).enter(65539, { value: ethers.utils.parseEther('0.01') })).to.be.reverted; // number too high
         });
     });
 
-    describe('First round ends with no winners', function () {
-        it('Should fail to enter specific round after the round closed', async function () {
-            // Move hre 1 round in the future
+    describe('First week ends with no winners', function () {
+        it('Should fail to enter', async function () {
+            // Move hre 1 week in the future
             let endTime = await guessNumberGameContract.endTime();
             await ethers.provider.send('evm_mine', [Number(endTime)]);
             await expect(guessNumberGameContract.connect(accounts[0]).enter(1, { value: ethers.utils.parseEther('0.001') })).to.be.reverted;
         });
 
         it('Close GuessNumberGame with no winners', async function () {
-            await guessNumberGameContract.closeRound(4);
+            await guessNumberGameContract.getWinningNumberAndCloseRound({ value: ethers.utils.parseEther('0.01') });
             expect(await guessNumberGameContract.round()).to.equal(2);
             const entries = await guessNumberGameContract.getEntriesForNumber(4, 1);
             expect(entries).to.be.empty;
@@ -65,48 +74,9 @@ describe('GuessNumberGame', function () {
             expect(pot).to.equal(ethers.utils.parseEther('0.002'));
         });
 
-        it('End time should push back 1 round from original end time', async function () {
-            let roundAfter = endTime + interval;
-            expect(await guessNumberGameContract.endTime()).to.equal(roundAfter);
-        });
-    });
-
-    describe('Second round', function () {
-        it('Users enter between 1-3', async function () {
-            for (let account of accounts) {
-                let randomNumber = Math.floor(Math.random() * 3);
-                await guessNumberGameContract.connect(account).enter(randomNumber, { value: ethers.utils.parseEther('0.0001') });
-                const entries = await guessNumberGameContract.getEntriesForNumber(randomNumber, 2);
-                expect(entries).to.include(account.address);
-            }
-        });
-
-        it('Choose winners', async function () {
-            const winningNumber = 2;
-
-            // Move hre 1 round in the future
-            let endTime = await guessNumberGameContract.endTime();
-            await ethers.provider.send('evm_mine', [Number(endTime)]);
-
-            const winners = await guessNumberGameContract.getEntriesForNumber(winningNumber, 2);
-            let balanceBefore = await ethers.provider.getBalance(winners[0]);
-            await guessNumberGameContract.closeRound(winningNumber);
-
-            const balanceAfter = await ethers.provider.getBalance(winners[0]);
-            expect(balanceAfter.gt(balanceBefore)).to.be.true;
-        });
-
-        it('Should move to round 3', async function () {
-            expect(await guessNumberGameContract.round()).to.equal(3);
-            expect(await guessNumberGameContract.pot()).to.equal(0);
-        });
-
-        it('Add funds sent to contract to pot', async function () {
-            await accounts[0].sendTransaction({
-                to: guessNumberGameContract.address,
-                value: ethers.utils.parseEther('1'),
-            });
-            expect(await guessNumberGameContract.pot()).to.equal(ethers.utils.parseEther('1'));
+        it('End time should push back 1 week from original end time', async function () {
+            let weekAfter = nextWeek + 86400;
+            expect(await guessNumberGameContract.endTime()).to.equal(weekAfter);
         });
     });
 });
